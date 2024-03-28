5e5940b294e2e75a6e30c6d79255f367e1537739
Michael Dopheide <dopheide@es.net>
2024-03-27 19:10:59 -0500

    is it event_data that's the problem?

--- translator/gobgp.py
+++ translator/gobgp.py
@@ -24,10 +24,12 @@ class GoBGP(object):
 
     def _build_path(self, ip, event_data):
 
-        # asn = event_data['message'].get("asn", 64500)
-        # community = event_data['message'].get("community", 666)
-        asn = 64500
-        community = 666
+        event_data = {"type": "translator_add", "message": {"route": "127.0.0.1/32"}}
+
+        asn = event_data['message'].get("asn", 64500)
+        community = event_data['message'].get("community", 666)
+        # asn = 64500
+        # community = 666
 
         origin = Any()
         origin.Pack(
